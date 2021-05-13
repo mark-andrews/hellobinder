113468aee6cde4a5a44ea1ec2b6fc53e932554c3
Mark Andrews <mjandrews.org@gmail.com>
2021-05-13 07:47:27 +0100

    Update the hello.R

--- hello.R
+++ hello.R
@@ -1,1 +1,8 @@
-print("Hello world")
+library(tidyverse)
+
+diamonds %>% 
+  ggplot(mapping = aes(x = carat, y = price, colour = clarity)) + 
+  geom_point() +
+  theme_classic() +
+  theme(legend.position = 'bottom') +
+  ggtitle('Diamonds are forever')
